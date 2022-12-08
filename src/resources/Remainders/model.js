97675bf4d2520fd57d499cc5873bb6922719bc1e
wubeZ <wubezeleke@gmail.com>
2022-12-09 00:14:03 +0300

    Created Remainder Schema

---
+++ src/resources/Remainders/model.js
@@ -0,0 +1,19 @@
+import mongoose, {Schema} from "mongoose";
+
+const RemainderSchema = new Schema({
+    createdBy: {type: Schema.Types.ObjectId, reqired: true},
+    content: {type: String, required: true},
+    setTime: {type: Number, required: true},
+    isCompleted: {type: Boolean, default: false, required: false},
+    completedTime: {type: Number, default: -1 }
+},
+{ timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+})
+
+RemainderSchema.set('toJSON',{virtuals: true})
+
+const remainderModel = mongoose.model('Remainders', RemainderSchema)
+
+module.exports = {
+    remainderModel
+}
